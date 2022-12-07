ea376bd0be4e257f2c62121b0163aff2ce9cd3e5
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-12-06 21:38:16 -0300

    Exercício 1 finalizado

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,33 @@
-// OLÁ MUNDO
+// EXERCICIO 1
+// const primeiroArray = []
+
+// calcularTabuada(5)
+
+// function calcularTabuada(num) {
+//     for (let i = 0; i <= 10; i++) {
+//         primeiroArray[i] = num * i
+//     }
+
+//     calcularTabuadaSeguinte(num)
+// }
+
+// function calcularTabuadaSeguinte(num) {
+//     const numeroMaisUm = num + 1
+//     const numeroMaisDois = num + 2
+
+//     for (let index = 0; index <= 10; index++) {
+//         primeiroArray.push(numeroMaisUm * index)
+//         primeiroArray.push(numeroMaisDois * index)
+//     }
+
+//     return primeiroArray
+// }
+// console.log(primeiroArray)
+// TERMINA AQUI
+
+// COMEÇA AQUI EXERCÍCIO 2
+
+const array = [[10,0], [3,5], [5,8]]
+
+
+
