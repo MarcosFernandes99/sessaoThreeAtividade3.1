5c58ee03bbc431e140c455feb6e85887fe9aec81
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-12-06 22:57:32 -0300

    Exercício 2 finalizado

--- javaScript.js
+++ javaScript.js
@@ -27,7 +27,33 @@
 
 // COMEÇA AQUI EXERCÍCIO 2
 
-const array = [[10,0], [3,5], [5,8]]
+let array = [[10,0], [3,5], [5,8]]
+
+pontosDeOnibus(array)
+
+function pontosDeOnibus (array){
+    let resultado = 0
+
+    array.forEach(x => {
+        resultado += x[0]- x[1]
+    })
+
+    console.log(resultado)
+    return resultado
+
+}
+
+// TERMINA AQUI
+
+
+
+
+
+
+   
+
+
+
 
 
 
