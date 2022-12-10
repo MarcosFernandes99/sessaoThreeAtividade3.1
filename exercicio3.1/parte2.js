6af1ed2edce05dc781cdc406537f10168e95d1e1
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-12-09 23:14:28 -0300

    função 5 finalizada

--- exercicio3.1/parte2.js
+++ exercicio3.1/parte2.js
@@ -1,9 +1,8 @@
 // FUNÇÃO 4
 
-let array = []
 let carros = [
     {
-        marca: 'Volkswagem',
+        marca: 'Volks',
         modelo: 'Gol',
         ano: 2020,
         valor: 20000,
@@ -11,38 +10,61 @@ let carros = [
         automatico: false
     },
     {
+        marca: 'Volks',
+        modelo: 'Gol',
+        ano: 2015,
+        valor: 20000,
+        cor: 'azul',
+        automatico: false
+    },
+    {
         marca: 'Chevrolet',
         modelo: 'Astra',
         ano: 2015,
-        valor: 25000,
-        cor: 'vermelho',
+        valor: 20000,
+        cor: 'azul',
         automatico: false
     }
 ];
+// pesquisarPorMarca(carros, "Chevrolet")
 
-//array.push(carros)
-pesquisarPorMarca(carros, "Chevrolet")
+// function pesquisarPorMarca(carros, marca) {
+//     let arrayRetorno = []
 
-function pesquisarPorMarca(carros, marca) {
-    let arrayRetorno = []
+//     carros.forEach(function (x) {
+//         if (x.marca === marca) {
+//             arrayRetorno.push(x)
+//         }
 
-    carros.forEach(function (x) {
-        if (x.marca === marca) {
-            arrayRetorno.push(x)
-        }
+//     })
 
-    })
+//     console.log(arrayRetorno)
+//     return arrayRetorno
 
-    console.log(arrayRetorno)
-
-}
+// }
 
 // TERMINA AQUI
 
 // FUNÇÃO 5
 
+buscarMarcasDisponiveis(carros)
+function buscarMarcasDisponiveis(objeto) {    
 
+    let arrayFinal = objeto.reduce(function (accumulator, current) {     
+        
+        if (!accumulator.includes(current.marca)) {
+             accumulator.push(current.marca)
+        }
 
+        return accumulator
 
+    }, [])
 
+    return arrayFinal
 
+    // arrayInicio = objeto.map(x => x.marca)
+    // console.log(arrayInicio)
+    // arrayFinal = [...new Set(arrayInicio)]
+    // console.log(arrayFinal)
+
+}
